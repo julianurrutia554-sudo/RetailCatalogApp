98d8bf9a0c0b47a73fade0193971fe20a064d192
Julian Urrutia <julianurrutia554@gmail.com>
2026-05-28 15:36:26 -0500

    implement domain layer components for fetching products

--- RetailCatalog/Domain/Entities/Product.swift
+++ RetailCatalog/Domain/Entities/Product.swift
@@ -5,3 +5,9 @@
 //  Created by Julian Urrutia on 28/05/26.
 //
 
+struct Product: Identifiable, Equatable {
+    let id: String
+    let name: String
+    let price: Double
+    let imageUrl: String
+}
