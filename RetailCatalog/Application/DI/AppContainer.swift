ada08bf876e269424f03d1cff185b7f88c0f25ee
Julian Urrutia <julianurrutia554@gmail.com>
2026-05-28 16:27:31 -0500

    register infrastructure, repository, use case, and view model factories

--- RetailCatalog/Application/DI/AppContainer.swift
+++ RetailCatalog/Application/DI/AppContainer.swift
@@ -5,3 +5,41 @@
 //  Created by Julian Urrutia on 28/05/26.
 //
 
+import FactoryKit
+
+extension Container {
+    
+    // MARK: - Capa de Infraestructura (Instancias Únicas / Singletons)
+    var dataTransferService: Factory<DataTransferServiceProtocol> {
+        self { DataTransferService() }.singleton
+    }
+    
+    var coreDataStorage: Factory<CoreDataStorageProtocol> {
+        self { CoreDataStorage.shared }.singleton
+    }
+    
+    var keychainStorage: Factory<KeychainStorageProtocol> {
+        self { KeychainStorage() }.singleton
+    }
+    
+    // MARK: - Capa de Datos (Data)
+    // El repositorio recibe la infraestructura de red y de persistencia local
+    var productsRepository: Factory<ProductsRepositoryProtocol> {
+        self {
+            DefaultProductsRepository(
+                networkService: self.dataTransferService(),
+                cacheStorage: self.coreDataStorage()
+            )
+        }
+    }
+    
+    // MARK: - Capa de Dominio (Domain)
+    var fetchProductsUseCase: Factory<FetchProductsUseCase> {
+        self { FetchProductsUseCase(repository: self.productsRepository()) }
+    }
+    
+    // MARK: - Capa de Presentación (Presentation)
+    var productListViewModel: Factory<ProductListViewModel> {
+        self { ProductListViewModel(fetchProductsUseCase: self.fetchProductsUseCase()) }
+    }
+}
